707584b26d24a9ded4b21ef7afed1950ced3a097
Eric Washington <eric.washington.me@gmail.com>
2021-03-21 02:41:08 -0400

    First initial Unit Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("should render a <div />", () => {
+    const container = shallow(<App />);
+    expect(container.find("div").length).toEqual(1);
+  });
 });
