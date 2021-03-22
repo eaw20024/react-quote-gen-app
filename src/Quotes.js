c64fc0ec7a892784027cdc827d01348f2ef9210a
Eric Washington <eric.washington.me@gmail.com>
2021-03-21 23:44:58 -0400

    Added styling to components

--- src/Quotes.js
+++ src/Quotes.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./Quotes.css";
 
-const Quotes = ({ quoteText, quoteAuthor, quoteGenre }) => {
+const Quotes = ({
+  quoteText,
+  quoteAuthor,
+  quoteGenre,
+  toggleLike,
+  liked,
+  id,
+}) => {
   return (
     <div className="quote-container">
       <div className="quote-row">
         <div className="quote">
-          <h2>{quoteText}</h2>
+          <h4>{quoteText}</h4>
         </div>
       </div>
       <div className="quote-data">
         <p className="quote-author">{quoteAuthor}</p>
-        <p className="quote-genre">{quoteGenre}</p>
       </div>
     </div>
   );
